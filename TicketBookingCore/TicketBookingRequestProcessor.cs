8f879054ff5fc9deab5975f6102cd141447c2bdd
Rebeccagh <rebecca.gren.hornfeldt.webd24jon@edu.tucsweden.se>
2025-01-30 11:12:22 +0100

    Uppdaterat så testerna blir gröna.

--- TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -3,8 +3,10 @@ namespace TicketBookingCore
 {
     public class TicketBookingRequestProcessor
     {
+        private readonly ITicketBookingRepository _ticketBookingRepository;
         public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
         {
+            _ticketBookingRepository = ticketBookingRepository;
         }
 
         public TicketBookingResponse Book(TicketBookingRequest request)
@@ -13,6 +15,15 @@ namespace TicketBookingCore
             {
                 throw new ArgumentNullException(nameof(request));
             }
+
+            //kod för att spara i databasen
+            _ticketBookingRepository.Save(new TicketBooking
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            });
+
             //refractor för att returnera en ny TicketBookingResponse
             return new TicketBookingResponse
             {
